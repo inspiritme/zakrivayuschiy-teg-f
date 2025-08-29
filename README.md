1aba22182dd1e34549138c13f929023d426de46e
Adlet Koshanov <adletkoshanov@gmail.com>
2025-08-30 01:57:16 +0500

    project init

---
+++ README.md
@@ -0,0 +1,1 @@
+https://github.com/inspiritme/zakrivayuschiy-teg-f
--- scripts/like.js
+++ scripts/like.js
@@ -8,22 +8,22 @@
 Если эти классы поменять в HTML, скрипт перестанет работать. Будьте аккуратны.
 */
 
-const likeHeartArray = document.querySelectorAll('.like-icon');
-const likeButtonArray = document.querySelectorAll('.card__like-button');
-const iconButtonArray = document.querySelectorAll('.card__icon-button');
+const likeHeartArray = document.querySelectorAll('.like-icon')
+const likeButtonArray = document.querySelectorAll('.card__like-button')
+const iconButtonArray = document.querySelectorAll('.card__icon-button')
 
 iconButtonArray.forEach((iconButton, index) => {
   iconButton.onclick = () =>
-    toggleIsLiked(likeHeartArray[index], likeButtonArray[index]);
-});
+    toggleIsLiked(likeHeartArray[index], likeButtonArray[index])
+})
 
 likeButtonArray.forEach((button, index) => {
-  button.onclick = () => toggleIsLiked(likeHeartArray[index], button);
-});
+  button.onclick = () => toggleIsLiked(likeHeartArray[index], button)
+})
 
 function toggleIsLiked(heart, button) {
-  heart.classList.toggle('is-liked');
-  setButtonText(heart, button);
+  heart.classList.toggle('is-liked')
+  setButtonText(heart, button)
 }
 
 function setButtonText(heart, button) {
@@ -31,11 +31,11 @@ function setButtonText(heart, button) {
     setTimeout(
       () => (button.querySelector('.button__text').textContent = 'Unlike'),
       500
-    );
+    )
   } else {
     setTimeout(
       () => (button.querySelector('.button__text').textContent = 'Like'),
       500
-    );
+    )
   }
 }
